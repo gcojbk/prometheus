e825e7480918f208c5bd40ff1308ba54e6473452
chaoguo <17625933942@163.com>
2021-06-27 22:29:56 +0800

    test

--- test/testAll.cc
+++ test/testAll.cc
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 
 int main(int argc, char *argv[]) {
-    ::testing::GTEST_FLAG(output) = "xml:Test_Pss_Asio.xml";
+    ::testing::GTEST_FLAG(output) = "xml:TestUtil.xml";
 
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
